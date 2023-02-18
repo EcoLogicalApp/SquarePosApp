2502ec1dd2e11216de5ec4c00942008295ed098a
Kuan Liu <kuanliu@Kuans-MBP.attlocal.net>
2023-02-18 12:04:44 -0800

    added getToken function

--- amplify/backend/function/oauthcallbacklambda/src/app.js
+++ amplify/backend/function/oauthcallbacklambda/src/app.js
@@ -7,67 +7,185 @@ const app = express();
 app.use(bodyParser.json());
 app.use(awsServerlessExpressMiddleware.eventContext());
 
-// Enable CORS for all methods
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
-  next();
+// initialize for square lib
+const dotenv = require("dotenv").config(); // Loads .env file. TODO: need to update env filess
+const cookieParser = require("cookie-parser");
+const md5 = require("md5");
+const { ApiError, Client, Environment } = require("square");
+app.use(cookieParser());
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+
+const { PORT, SQ_ENVIRONMENT, SQ_APPLICATION_ID, SQ_APPLICATION_SECRET } =
+  process.env;
+
+let basePath;
+let environment;
+if (SQ_ENVIRONMENT.toLowerCase() === "production") {
+  basePath = `https://connect.squareup.com`;
+  environment = Environment.Production;
+} else if (SQ_ENVIRONMENT.toLowerCase() === "sandbox") {
+  basePath = `https://connect.squareupsandbox.com`;
+  environment = Environment.Sandbox;
+} else {
+  console.warn("Unsupported value for SQ_ENVIRONMENT in .env file.");
+  process.exit(1);
+}
+
+const messages = require("./messages"); //TODO: need to update message file
+
+// Configure Square defcault client
+const squareClient = new Client({
+  environment: environment,
+  userAgentDetail: "sample_app_oauth_node", // Remove or replace this detail when building your own app
 });
+
+// Configure Square OAuth API instance
+const oauthInstance = squareClient.oAuthApi;
+
+// // Enable CORS for all methods
+// // Check this and see if we need to disable this.
+// // https://spring.io/guides/gs/rest-service-cors/
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "*");
+//   next();
+// });
 
 /**********************
- * Example get method *
+ * get method *
  **********************/
 
-app.get("/callback", function (req, res) {
-  // Add your code here
-  res.json({ success: "get call succeed!", url: req.url });
-});
+app.get("/callback", async (req, res) => {
+  console.log(req.query);
+  // // Verify the state to protect against cross-site request forgery.
+  // if (req.cookies["Auth_State"] !== req.query["state"]) {
+  //   content = messages.displayStateError();
+  //   res.render("base", {
+  //     content: content,
+  //   });
+  // } else
 
-app.get("/callback/*", function (req, res) {
-  // Add your code here
-  res.json({ success: "get call succeed!", url: req.url });
-});
+  if (req.query["error"]) {
+    console.log("=========== in req.query['error'] =========");
 
-/****************************
- * Example post method *
- ****************************/
+    // Check to see if the seller clicked the Deny button and handle it as a special case.
+    if (
+      "access_denied" === req.query["error"] &&
+      "user_denied" === req.query["error_description"]
+    ) {
+      res.render(
+        messages.displayError(
+          "Authorization denied",
+          "You chose to deny access to the app."
+        )
+      );
+      // res.json("Authorization denied. You chose to deny access to the app.");
+    }
+    // Display the error and description for all other errors.
+    else {
+      content = messages.displayError(
+        req.query["error"],
+        req.query["error_description"]
+      );
+      res.render("base", {
+        content: content,
+      });
+      // res.json(req.query["error"], req.query["error_description"]);
+    }
+  }
+  // When the response_type is "code", the seller clicked Allow
+  // and the authorization page returned the auth tokens.
+  else if ("code" === req.query["response_type"]) {
+    console.log(
+      "=========== in 'code' === req.query['response_type'] ========="
+    );
+    // Extract the returned authorization code from the URL
+    var { code } = req.query;
 
-app.post("/callback", function (req, res) {
-  // Add your code here
-  res.json({ success: "post call succeed!", url: req.url, body: req.body });
-});
+    try {
+      console.log("=========== before call obtainToken =========");
+      console.log("==code==", code);
+      console.log(
+        "==process.env.SQ_APPLICATION_ID==",
+        process.env.SQ_APPLICATION_ID
+      );
+      console.log(
+        "==process.env.SQ_APPLICATION_SECRET==",
+        process.env.SQ_APPLICATION_SECRET
+      );
 
-app.post("/callback/*", function (req, res) {
-  // Add your code here
-  res.json({ success: "post call succeed!", url: req.url, body: req.body });
-});
+      let { result } = await oauthInstance.obtainToken({
+        // Provide the code in a request to the Obtain Token endpoint
+        code,
+        clientId: process.env.SQ_APPLICATION_ID,
+        clientSecret: process.env.SQ_APPLICATION_SECRET,
+        grantType: "authorization_code",
+      });
 
-/****************************
- * Example put method *
- ****************************/
+      console.log("=========== before extract value =========");
+      let {
+        // Extract the returned access token from the ObtainTokenResponse object
+        accessToken,
+        refreshToken,
+        expiresAt,
+        merchantId,
+      } = result;
 
-app.put("/callback", function (req, res) {
-  // Add your code here
-  res.json({ success: "put call succeed!", url: req.url, body: req.body });
-});
+      // Because we want to keep things simple and we're using Sandbox,
+      // we call a function that writes the tokens to the page so we can easily copy and use them directly.
+      // In production, you should never write tokens to the page. You should encrypt the tokens and handle them securely.
+      content = messages.writeTokensOnSuccess(
+        accessToken,
+        refreshToken,
+        expiresAt,
+        merchantId
+      );
 
-app.put("/callback/*", function (req, res) {
-  // Add your code here
-  res.json({ success: "put call succeed!", url: req.url, body: req.body });
-});
+      console.log("=========== before render =========");
 
-/****************************
- * Example delete method *
- ****************************/
-
-app.delete("/callback", function (req, res) {
-  // Add your code here
-  res.json({ success: "delete call succeed!", url: req.url });
-});
-
-app.delete("/callback/*", function (req, res) {
-  // Add your code here
-  res.json({ success: "delete call succeed!", url: req.url });
+      res.render("base", {
+        content: content,
+      });
+      // res.json({ accessToken, refreshToken, expiresAt, merchantId });
+    } catch (error) {
+      // The response from the Obtain Token endpoint did not include an access token. Something went wrong.
+      if (error instanceof ApiError) {
+        // content = messages.displayError(
+        //   "Exception",
+        //   JSON.stringify(error.result)
+        // );
+        // res.render("base", {
+        //   content: content,
+        // });
+        console.log(
+          "JSON.stringify(error.result)",
+          JSON.stringify(error.result)
+        );
+        res.json({
+          exception: "Exception!",
+          messages: JSON.stringify(error.result),
+        });
+      } else {
+        // content = messages.displayError("Exception", JSON.stringify(error));
+        // res.render("base", {
+        //   content: content,
+        // });
+        console.log("JSON.stringify(error)", JSON.stringify(error));
+        res.json({ exception: "Exception!", messages: JSON.stringify(error) });
+      }
+    }
+  } else {
+    // No recognizable parameters were returned.
+    content = messages.displayError(
+      "Unknown parameters",
+      "Expected parameters were not returned"
+    );
+    res.render("base", {
+      content: content,
+    });
+    // res.json({ content });
+  }
 });
 
 app.listen(3000, function () {
